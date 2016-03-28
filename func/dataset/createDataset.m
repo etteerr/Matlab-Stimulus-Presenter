aaaa650438855f57a787587879bfa9abc6e49ce1
Shanshi <etteerr@gmail.com>
2016-03-28 16:01:09 +0200

    Dataset Editor
    -Created commandline interface for datasets
    - Changed menu directory layout
    - Changed eventEditor.m => questionDialog.m
    - Update.cmd broken
    - Created databaseEditor GUI
    
    
    Former-commit-id: 20b4eaf6058fd0524c08d675dc7a21bec626c7fa [formerly 20b4eaf6058fd0524c08d675dc7a21bec626c7fa [formerly 693bb57596f9e007706d60a7a7347ee1338c8e68]]
    Former-commit-id: a333f8775789958da3b509909930a5233266300b
    Former-commit-id: 7a33b9c74e745fee58fdb0ee6c2239f65ac84892

--- func/dataset/createDataset.m
+++
@@ -1,7 +0,0 @@
-function [ output_args ] = createDataset( input_args )
-%CREATEDATASET Summary of this function goes here
-%   Detailed explanation goes here
-
-
-end
-
--- func/dataset/validateDataset.m
+++
@@ -1,7 +0,0 @@
-function [ output_args ] = validateDataset( input_args )
-%VALIDATEDATASET Summary of this function goes here
-%   Detailed explanation goes here
-
-
-end
-
